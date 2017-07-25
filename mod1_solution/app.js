ff24a0e36b247db45e6d26cb9d56e625fcc5c0de
Leon Wee <leonweecs@gmail.com>
2017-07-26 01:48:34 +0800

    Added color indicator

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -1,31 +1,34 @@
 (function(){
   'use strict';
 
-  var app = angular.module('lunchChecker',[]);
+  var app = angular.module('LunchCheck',[]);
 
-  app.controller('lunchCheckerCtrl',lunchCheckerCtrl);
+  app.controller('LunchCheckControl',LunchCheckControl);
 
-  lunchCheckerCtrl.$inject = ['$scope'];
+  LunchCheckControl.$inject = ['$scope'];
 
-  function lunchCheckerCtrl($scope){
+  function LunchCheckControl($scope){
     $scope.menu = "";
-
+    $scope.status = "";
     $scope.message = "";
 
     $scope.check = function(){
       if($scope.menu===""){
         $scope.message = "Please enter data first";
+        $scope.status = "has-error";
         return;
       }
       var items = $scope.menu.split(",");
       if(items.length <= 3){
         $scope.message = "Enjoy!";
+        $scope.status = "has-success";
       }
       else {
         $scope.message = "Too much!";
+        $scope.status = "has-success";
       }
     };
 
   };
-  
+
 })();
